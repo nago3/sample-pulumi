fd31df7aeb37b9c1e7fa32be54cd3383823f930f
nago3 <hiro@nago3.xyz>
2023-12-23 06:39:55 +0900

    change: Update config setting

--- api_gateway/__main__.py
+++ api_gateway/__main__.py
@@ -4,15 +4,17 @@ import pulumi
 import pulumi_aws as aws
 import pulumi_aws_apigateway as apigateway
 
+# Get the current stack reference
+config = pulumi.Config()
+env = config.require("env") or "dev"
+
 # Load swagger file
 with open('data/swagger-apigateway-sample.json', 'r', encoding='utf-8') as f:
     api = json.load(f)
 
 # Define whole API using swagger (OpenAPI)
-swagger_api = apigateway.RestAPI(
-    "swagger-api",
+swagger_api = apigateway.RestAPI("swagger-api",
     swagger_string=json.dumps(api),
-    stage_name="dev"
-)
+    stage_name=env)
 
 pulumi.export("swagger-url", swagger_api.url)
